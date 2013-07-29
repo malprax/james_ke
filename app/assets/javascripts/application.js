aabd046690d8f713994c229521080d50eeeb8429
aulia <kingmalprax@gmail.com>
2013-07-29 23:50:45 +0800

    fix template Flat_UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.ui.touch-punch.min
+//= require bootstrap.min
+//= require bootstrap-select
+//= require bootstrap-switch
+//= require flatui-checkbox
+//= require flatui-radio
+//= require jquery.tagsinput
+//= require jquery.placeholder
+//= require jquery.stacktable
+//= require google-code-prettify/prettify
+//= require styling
 //= require_tree .
