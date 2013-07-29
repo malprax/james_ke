c61b9cd3f8dc20e1b25338de814929d892220aa0
aulia <kingmalprax@gmail.com>
2013-07-29 20:27:50 +0800

    first step ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamesKe::Application.config.secret_token = '7107c471abd7cbf88184e3b1369aa23bef7692287eef2909689918e79f17a2e97c3c4588d2cbdca25296b01c164fc3f892ea74d3f7922721f56b6c037634d97c'
